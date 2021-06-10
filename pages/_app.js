73bcf92768d17787d5f96e9e39ecc80229f776fb
Nobbele <realnobbele@gmail.com>
2021-06-10 22:18:22 +0200

    Adds context

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
+import { AppWrapper } from '../components/appWrapper';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AppWrapper>
+      <Component {...pageProps} />
+    </AppWrapper>
+  );
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,21 @@
+import { useAppContext } from '../components/appWrapper';
 import NavEntry from '../components/NavEntry'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const [sharedState, setSharedState] = useAppContext();
+
   return (
     <div className={styles.container}>
-      <div className={styles.content}>
-
-      </div>
       <div className={styles.sidebar}>
         <h2>nobbele's Website</h2>
         <nav className={styles.nav}>
           <NavEntry title="Home" href="/" />
         </nav>
+      </div>
+      <div className={styles.content}>
+        <p>{sharedState.darkMode ? "YES" : "NO"}</p>
+        <button onClick={() => setSharedState({ ...sharedState, darkMode: !sharedState.darkMode })}>Test</button>
       </div>
     </div>
   )
