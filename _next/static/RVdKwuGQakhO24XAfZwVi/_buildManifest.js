2b606669649f10d0795353c81c9380a671d2e0d9
nobbele <realnobbele@gmail.com>
2021-06-28 21:19:34 +0000

    Deploying to gh-pages from  @ 4fdc47ea09268578d0a9c80120791054be868211 🚀

--- _next/static/RVdKwuGQakhO24XAfZwVi/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/427e2f4562b1d6d63e7f.css","static/chunks/pages/index-e6cd7d0c1e2c80f08219.js"],"/_error":["static/chunks/pages/_error-b30902e13465df7c5366.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
